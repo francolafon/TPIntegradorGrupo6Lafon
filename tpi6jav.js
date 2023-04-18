c2289e538dfd45a1c89957ac321bf27488ecb0f4
Franco Lafon <francolafon@gmail.com>
2023-04-18 04:35:47 -0300

    generador de pdf funcionando ok

--- tpi6jav.js
+++ tpi6jav.js
@@ -19,3 +19,39 @@ window.addEventListener("scroll", function() {
       elemento.classList.add("selected");
     });
   });
+
+//boton ENVIAR
+
+function enviarDatos(){
+     var doc = new jsPDF('p', 'pt','letter');
+     var margin = 10;
+     var scale = (doc.internal.pageSize.width - margin *2) / document.body.scrollWidth;
+     doc.html(document.body, {
+      x:margin,
+      y: margin,
+      html2canvas: {
+        scale: scale,
+
+      },
+      callback: function(doc){
+        doc.output('dataurlnewwindow', {filename: 'fichero-pdf.pdf'})
+      }
+     })
+}
+//boton IMPRIMIR ORDEN
+function imprimirDatos(){
+  var doc = new jsPDF('p', 'pt','letter');
+  var margin = 10;
+  var scale = (doc.internal.pageSize.width - margin *2) / document.body.scrollWidth;
+  doc.html(document.body, {
+   x:margin,
+   y: margin,
+   html2canvas: {
+     scale: scale,
+
+   },
+   callback: function(doc){
+     doc.save("formulario.pdf")
+   }
+  })
+}
